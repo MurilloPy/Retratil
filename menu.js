d2e314f6effc70bae4eceb5a5f5ddb38bd24ea18
MurilloPy <MurilloServico@gmail.com>
2023-08-09 01:15:17 -0300

    1.0

--- menu.js
+++
@@ -1,9 +0,0 @@
-const Hamburger = document.querySelector(".hamburger");
-const Menu = document.querySelector(".menu");
-
-Hamburger.addEventListener("click", () => {
-
-    Hamburger.classList.toggle("open");
-    Menu.classList.toggle("show-menu");
-
-}); 
